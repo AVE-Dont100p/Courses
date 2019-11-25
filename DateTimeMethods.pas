236698bf5e497cea6b65faa96a54de45fe005efd
SergeyMir <miroshn14@mail.ru>
2019-11-25 17:10:39 +0300

    hard

---
+++ DateTimeMethods.pas
@@ -0,0 +1,21 @@
+﻿unit DateTimeMethods;
+interface
+function IsLeapYear(year: integer): boolean;
+function DaysInMonth(month, year: integer): integer := 0;
+function LaterInDay(p1, p2: DateTime): DateTime := p1;
+function LaterInYear(p1, p2: DateTime): DateTime := p1;
+function DaysInYear(year: integer): integer := 0;
+function DaysInYearRange(year1, year2: integer): integer := 0;
+function SecondsInHours(hours: integer): integer := 0;
+implementation
+function IsLeapYear(year: integer): boolean;
+begin
+if (year mod 4) IsLeapYear := true
+else IsLeapYear := false;
+end;
+function DaysInMonth(month, year: integer): integer;
+begin
+  case month of
+    
+  end;
+end;
