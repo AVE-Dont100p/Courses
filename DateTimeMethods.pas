2886d63e6d1a5205d9aba42121b9e664e1d0335d
Мирошниченко Сергей Александрович <sermir@sfedu.ru>
2019-12-02 16:34:48 +0300

    400Year

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -12,7 +12,7 @@ implementation
 
 function IsLeapYear(year: integer): boolean;
 begin
-if (year mod 4) IsLeapYear := true
+if ((year mod 4 = 0) and not(year mod 100 = 0)) or (year mod 400 = 0) IsLeapYear := true
 else IsLeapYear := false;
 end;
 
